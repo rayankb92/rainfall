0b37c0220e3ffd3e09eb52fc02e5aa223f2011b2
rayan ferradi <ferradi.r@gmail.com>
2024-11-13 20:30:33 +0100

    level 2 en cours

---
+++ level0/source.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    // Convertit le deuxième argument en entier
+    int input_value = atoi(argv[2]);
+
+    // Vérifie si l'entrée est égale à 423
+    if (input_value == 423) {
+        // Duplique la chaîne "/bin/sh"
+        char *shell_path = strdup("/bin/sh");
+
+        // Obtient les UID et GID effectifs
+        uid_t effective_uid = geteuid();
+        gid_t effective_gid = getegid();
+
+        // Définit les UID et GID effectifs du processus
+        setresgid(effective_gid, effective_gid, effective_gid);
+        setresuid(effective_uid, effective_uid, effective_uid);
+
+        // Exécute le shell
+        execv(shell_path, &shell_path);
+    } else {
+        // Affiche un message d'erreur si l'entrée n'est pas 423
+        fwrite("No !\n", 1, 5, stderr);
+    }
+
+    return 0;
+}
+fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
---
+++ level2/test.py
@@ -0,0 +1,3 @@
+from pwn import *
+pattern = cyclic(100)  # Génère une chaîne de 100 caractères unique
+print(pattern)
